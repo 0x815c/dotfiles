f9b3456e2cec6e8ff2ac46622f21efaecc9d1d7b
Shinichi Yamashita <dev@snicy.me>
2022-10-23 08:34:09 +0900

    :wrench: edit installer

--- auto_install.sh
+++ auto_install.sh
@@ -14,17 +14,7 @@ if [ ! -d ${DOT_DIR} ]; then
         exit 1
     fi
     cd ${DOT_DIR}
-    for f in *
-    do
-        [[ "$f" == ".git" ]] && continue
-        [[ "$f" == ".gitignore" ]] && continue
-        [[ "$f" == ".DS_Store" ]] && continue
-        [[ "$f" == "README.md" ]] && continue
-        [[ "$f" == "install.sh" ]] && continue
-
-        ln -snf $DOT_DIR/"$f" $HOME/"$f"
-        echo "Installed .$f"
-    done
+    install.sh
 else
     echo "dotfiles already exists"
     exit 1
